14e9a68be882349d377942de62294984bb061644
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-05-17 21:58:09 +0800

    fix: remove unused components and simplify head and nav function calls

--- index.php
+++ index.php
@@ -1,22 +1,17 @@
 <?php
 require BASE_PATH . '/vendor/autoload.php';
-require BASE_PATH . '/utils/htmlEscape.utils.php';
 
 require_once BASE_PATH . '/handlers/pageData.handler.php';
-require_once BASE_PATH . '/components/cards/note.component.php';
 require_once BASE_PATH . '/components/templates/foot.component.php';
 require_once BASE_PATH . '/components/templates/head.component.php';
 require_once BASE_PATH . '/components/templates/nav.component.php';
 ?>
 
 <?php
-head('Product List');
-navHeader($navList);
+head();
+nav($menu);
 ?>
 
-<section class="grid grid-cols-3">
-    <?php displayCardForm($productList); ?>
-</section>
 
 <?php
 footer();
