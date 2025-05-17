97390254e947aac909bf14e6975400c67db4f5ca
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-05-17 23:05:31 +0800

    refactor: update section and div classes for better styling in displaySection function

--- utils/displaySection.utils.php
+++ utils/displaySection.utils.php
@@ -5,9 +5,9 @@ require_once BASE_PATH . '/utils/displayPets.utils.php';
 <?php
 function displaySection($categories){
         foreach ($categories as $categoryName => $pets) {
-        echo "<section>";
+        echo '<section class="pets-section">';
         echo "<h3>" . ucfirst($categoryName) . "</h3>";
-        echo '<div class="pets-section">';
+        echo '<div class="pets">';
         displayPets($pets, $categoryName);
         echo "</div>";
         echo "</section>";
